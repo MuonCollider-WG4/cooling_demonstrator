1ed5180fe4430e52869b3532ae784a8faf04be25
Chris Rogers <chris.rogers@stfc.ac.uk>
2025-08-01 14:20:50 +0100

    Add some comments

--- test/test_cooling.py
+++ test/test_cooling.py
@@ -7,7 +7,13 @@ import unittest
 import sys
 
 class TestCooling(unittest.TestCase):
+    """
+    Test the cooling lattice is okay
+
+    For now just check it runs; eventually we can check performance etc
+    """
     def setUp(self):
+        """Make a tempdir to run the test in."""
         self.tempdir = tempfile.TemporaryDirectory()
         self.test_path = os.path.abspath(__file__)
         self.g4bl_cooling_dir = pathlib.Path(self.test_path).parents[1]
@@ -17,12 +23,13 @@ class TestCooling(unittest.TestCase):
         shutil.copytree(self.g4bl_cooling_dir, self.target_dir)
 
     def tearDown(self):
+        """Clean up tempdir and log file resources"""
         print("Ran in", self.target_dir)
         self.log_file.close()
         self.tempdir.cleanup()
 
-    def test_runs(self):
-        """Check that the test runs and returns 0"""
+    def test_g4bl_cooling_runs(self):
+        """Check that the G4BL cooling simulation runs and returns 0"""
         os.chdir(self.target_dir)
         command = [os.path.expandvars("${G4BL_EXE_PATH}/g4bl"), "cooling.g4bl", "last_event=10"]
         output = subprocess.run(command, stdout=self.log_file, stderr=subprocess.STDOUT)
